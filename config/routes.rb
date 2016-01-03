596c30b48db940bb3944baa11bb829992308cd3c
Kevin Korte <kevinkorte15@gmail.com>
2016-01-03 02:14:31 -0800

    basic install of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :members
+  get 'pages/welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#welcome'
+
+  resources :dashboard
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
